f04a7258f481808b3914fb4342c2cae75c6b457d
Adorigo <agustin.dorigo@gmail.com>
2016-03-05 22:37:45 -0300

    05/03/2016 22:37

--- app/src/main/java/ar/org/ineco/prl/programaderehabilitaciondellenguaje/database/MyDatabase.java
+++ app/src/main/java/ar/org/ineco/prl/programaderehabilitaciondellenguaje/database/MyDatabase.java
@@ -8,12 +8,12 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class MyDatabase extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "bd_prdl.db";
+    private static final String DATABASE_NAME = "bd_prdl.sqlite";
 
     private static final int DATABASE_VERSION = 1;
 
     public static final String TABLE_OPTION = "opcion";
-    public static final String OPTION_COLUMN_ID = "opc_id";
+    public static final String OPTION_COLUMN_ID = "_id";
     public static final String OPTION_COLUMN_SNDID = "opc_snd";
     public static final String OPTION_COLUMN_IMGID = "opc_img";
     public static final String OPTION_COLUMN_TXT = "opc_txt";
@@ -21,43 +21,40 @@ public class MyDatabase extends SQLiteAssetHelper {
     public static final String OPTION_COLUMN_QID = "preg_id";
 
     public static final String TABLE_QUESTION = "pregunta";
-    public static final String QUESTION_COLUMN_ID = "preg_id";
+    public static final String QUESTION_COLUMN_ID = "_id";
     public static final String QUESTION_COLUMN_TXT = "preg_consignatxt";
     public static final String QUESTION_COLUMN_CHECK = "preg_check";
     public static final String QUESTION_COLUMN_SND = "preg_consignasnd";
-    public static final String QUESTION_COLUMN_QID = "preg_padre_id";
+    public static final String QUESTION_COLUMN_QID = "preg_id";
     public static final String QUESTION_COLUMN_LVLID = "niv_id";
 
     public static final String TABLE_PREGIMG = "preg_img";
-    public static final String PREGIMG_COLUMN_ID = "pregimg_id";
+    public static final String PREGIMG_COLUMN_ID = "_id";
     public static final String PREGIMG_COLUMN_QID = "preg_id";
     public static final String PREGIMG_COLUMN_IMGID = "img_id";
 
     public static final String TABLE_REGUSE = "registro_de_uso";
-    public static final String REGUSE_COLUMN_ID = "ru_id";
+    public static final String REGUSE_COLUMN_ID = "_id";
     public static final String REGUSE_COLUMN_QID = "preg_id";
     public static final String REGUSE_COLUMN_CORR = "ru_correcto";
 
     public static final String TABLE_SND = "sonido";
-    public static final String SND_COLUMN_ID = "snd_id";
+    public static final String SND_COLUMN_ID = "_id";
     public static final String SND_COLUMN_NAME = "snd_nombre";
 
-    public static final String TABLE_MODULE = "modulo";
-    public static final String MODULE_COLUMN_ID = "mod_id";
-    public static final String MODULE_COLUMN_NAME = "mod_nombre";
-
     public static final String TABLE_CATEGORY = "categoria";
-    public static final String CATEGORY_COLUMN_ID = "cat_id";
+    public static final String CATEGORY_COLUMN_ID = "_id";
     public static final String CATEGORY_COLUMN_NAME = "cat_nombre";
-    public static final String CATEGORY_COLUMN_CID = "cat_padre";
+    public static final String CATEGORY_COLUMN_CID = "cat_id";
 
     public static final String TABLE_IMG = "imagen";
-    public static final String IMG_COLUMN_ID = "img_id";
+    public static final String IMG_COLUMN_ID = "_id";
     public static final String IMG_COLUMN_NAME = "img_nombre";
 
     public static final String TABLE_LVL = "nivel";
-    public static final String LVL_COLUMN_ID = "niv_id";
+    public static final String LVL_COLUMN_ID = "_id";
     public static final String LVL_COLUMN_NUMBER = "niv_num";
+    public static final String LVL_COLUMN_LVLDONE = "niv_term";
     public static final String LVL_COLUMN_CID = "cat_id";
 
 
@@ -71,8 +68,6 @@ public class MyDatabase extends SQLiteAssetHelper {
     public static String[] allPregimgColumns = {PREGIMG_COLUMN_ID, PREGIMG_COLUMN_QID, PREGIMG_COLUMN_IMGID};
 
     public static String[] allREGUSEColumns = {REGUSE_COLUMN_ID, REGUSE_COLUMN_QID, REGUSE_COLUMN_CORR};
-
-    public static String[] allModuleColumns = {MODULE_COLUMN_ID, MODULE_COLUMN_NAME};
 
     public static String[] allCategoryColumns = {CATEGORY_COLUMN_ID, CATEGORY_COLUMN_NAME, CATEGORY_COLUMN_CID};
 
